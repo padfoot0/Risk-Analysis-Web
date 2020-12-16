c67b45fddc3096bddff11bcccfa977f44e6389de
padfoot <padfoot@localhost.localdomain>
2020-12-16 16:01:53 +0530

    in test.py i added login page

--- text.py
+++ text.py
@@ -1,9 +1,37 @@
 from flask import Flask , render_template , request , make_response
+from flask.helpers import send_file
 from flask.wrappers import Response
+import connection
+from connection import registerDetail,loginDetails
 app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("login.html")
+    return render_template("index.html")
+
+
+@app.route('/' , methods = ['GET' , 'POST'])
+def login():
+    emId = request.form['empId']
+    password = request.form['password']
+    result = loginDetails(emId , password)
+    if result == False:
+        return "False"
+    else:
+        return  render_template('main.html')
+
+# @app.route('/register' , methods = ['GET' , 'POST'])
+# def register():
+#     name = request.form['name']
+#     eId = request.form['eId']
+#     password = request.form['password']
+#     password1 = request.form['password1']
+#     print(name , eId , password)
+#     if password == password1:
+#         registerDetail(name , eId , password)
+#     else:
+#         print("data inserted")
+
+
 
 app.run(debug= True)
